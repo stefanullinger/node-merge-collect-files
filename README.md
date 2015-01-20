b60414c04756ef71e7d72ebd31b9cec88a1d4a05
Stefan Ullinger <stefan@slicemenice.de>
2015-01-20 10:32:21 +0100

    Added basic mergeCollectFiles function.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # node-merge-collect-files
+
+## Description
+
 Node module to merge two or more directories and collect the filenames
+
+
+
+## Usage
+
+To install this module in your current working directory (which should already contain a package.json), run
+
+```
+npm install node-merge-collect-files
+```
+
+You can additionally just list the module in your [package.json](https://npmjs.org/doc/json.html) and run npm install.
+
+Then, require this package where you need it:
+
+```
+var mergeCollectFiles = require('node-merge-collect-files');
+```
+
+The syntax for collecting merged files of two or more directories is as follows:
+
+```
+var files = mergeCollectFiles( [ 'directory-one', 'directory-two' ], '**/*.json' );
+```
---
+++ index.js
@@ -0,0 +1,61 @@
+var fs = require( 'fs' );
+var path = require( 'path' );
+var extend = require('node.extend');
+var glob = require( 'glob' );
+
+
+/**
+ * Get files of a given path
+ */
+function getPathFiles( basePath, pattern ) {
+	pattern = pattern || '**/*';
+
+	var files = {};
+
+	try {
+		var basePathStat = fs.lstatSync( basePath );
+
+		if ( !basePathStat.isDirectory() ) {
+			throw new Error( '"' + basePath + '" is not a directory.' );
+		}
+
+	} catch( e ) {
+		throw new Error( '"' + basePath + '" does not exist.' );
+	}
+
+	var filesMatchingPattern = glob.sync( pattern, {
+		cwd: basePath,
+		nonull: false
+	} );
+
+	filesMatchingPattern.forEach( function( file ) {
+		if( fs.lstatSync( path.join( basePath, file ) ).isFile() ) {
+			files[ file ] = {
+				absolutePath: path.join( basePath, file ),
+				basePath: basePath,
+				extension: path.extname( file ),
+				dirname: path.dirname( file ),
+				name: path.basename( file, path.extname( file ) )
+			};
+		}
+	});
+
+	return files;
+}
+
+function mergeCollectFiles( directories, pattern ) {
+	var result = {};
+
+	directories.forEach( function( directory ) {
+		var foundFiles = getPathFiles( directory, pattern );
+		console.log( 'Files in "' + directory + '": \n', foundFiles, '\n' );
+
+		result = extend( result, foundFiles );
+	});
+
+	console.log( 'Merged Files: \n', result, '\n' );
+
+	return result;
+}
+
+module.exports = mergeCollectFiles;
